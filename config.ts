2eae9a3fc376929dfb71dd2315787644a4fc3977
Harshad <harsh.haou@gmail.com>
2021-06-09 15:04:58 +0530

    Changes

--- config.ts
+++ config.ts
@@ -16,7 +16,7 @@ const siteMetadata = {
           At the time I'm at Webdura, I've worked with both web and mobile using React and React-native. React ecosystem is growing so rapidly, 
           which I'm so excited about. Tools like Gatsby and Next is enabling this new kind of web. From the development experience to great performance,
           good SEO, rich and delightful user experience.It's never been a better time to be a front end developer. I'm looking to be a part of team that
-        adapts to new changes and make use of it.`,
+        adapts to new changes and make use of it`,
     author: ``,
     blogItemsPerPage: 10,
     portfolioItemsPerPage: 10,
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,11 +8,11 @@ function index({data}) {
                     About
                 </h2>
                 <p className="mt-5 text-lg">
-                `I'm Harshad. I'm a React, Javascript enthusiast. I was working as a front end developer for the last 10 months in Webdura technologies.
+                I'm Harshad. I'm a React, Javascript enthusiast. I was working as a front end developer for the last 10 months in Webdura technologies.
                 At the time I'm at Webdura, I've worked with both web and mobile using React and React-native. React ecosystem is growing so rapidly, 
                 which I'm so excited about. Tools like Gatsby and Next is enabling this new kind of web. From the development experience to great performance,
                 good SEO, rich and delightful user experience.It's never been a better time to be a front end developer. I'm looking to be a part of team that
-                adapts to new changes and make use of it.`
+                adapts to new changes and make use of it.
                 </p>
             </div>
         </div>
